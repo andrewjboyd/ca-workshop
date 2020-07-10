a9963bde4c0c2aad6c9397ca78a3f8702a974a16
andrew-boyd <ab@ajwebsolutions.com>
2020-07-10 10:15:09 +1000

    Step 9

--- CaWorkshop/CaWorkshop.Application/DependencyInjection.cs
+++ CaWorkshop/CaWorkshop.Application/DependencyInjection.cs
@@ -1,7 +1,10 @@
 ﻿using AutoMapper;
+using CaWorkshop.Application.Common.Behaviours;
+using FluentValidation;
 using MediatR;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
 using System.Reflection;
 
 namespace CaWorkshop.Application
@@ -14,6 +17,40 @@ namespace CaWorkshop.Application
         {
             services.AddMediatR(Assembly.GetExecutingAssembly());
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            services.AddTransient(typeof(IPipelineBehavior<,>),
+                typeof(ValidationBehavior<,>));
+
+            services.AddAllRequestValidators();
+
+            return services;
+        }
+
+        private static IServiceCollection AddAllRequestValidators(
+            this IServiceCollection services)
+        {
+            var validatorType = typeof(IValidator<>);
+
+            var validatorTypes = Assembly.GetExecutingAssembly()
+                .GetExportedTypes()
+                .Where(t => t.GetInterfaces().Any(i =>
+                    i.IsGenericType &&
+                    i.GetGenericTypeDefinition() == validatorType))
+                .ToList();
+
+            foreach (var validator in validatorTypes)
+            {
+                var requestType = validator.GetInterfaces()
+                    .Where(i => i.IsGenericType &&
+                        i.GetGenericTypeDefinition() == typeof(IValidator<>))
+                    .Select(i => i.GetGenericArguments()[0])
+                    .First();
+
+                var validatorInterface = validatorType
+                    .MakeGenericType(requestType);
+
+                services.AddTransient(validatorInterface, validator);
+            }
 
             return services;
         }
